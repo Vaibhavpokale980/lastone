88c537d8a08851b373ec2faff3af69091c495805
Vaibhavpokale980 <128890407+Vaibhavpokale980@users.noreply.github.com>
2024-10-12 01:11:32 +0530

    Update connectDB.js

--- DB/connectDB.js
+++ DB/connectDB.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 // connecting to database
 const connectDB = async () => {
     const connectionUrl = process.env.DB_URI;
-    mongoose.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect('mongodb+srv://careerhub:Vaibhav%401234@cluster0.2vvul.mongodb.net/careerhub?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => console.log(`Database connected successfully`))
         .catch((err) => console.log("Getting Error from DB connection" + err.message))
     mongoose.set('strictQuery', false);
