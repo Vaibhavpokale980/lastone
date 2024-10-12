fe8e6e7abdae00481fd760576a4f75a84e7d37fa
Vaibhavpokale980 <128890407+Vaibhavpokale980@users.noreply.github.com>
2024-10-12 13:21:52 +0530

    Update login.js

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -4,7 +4,6 @@ import Joi from 'joi';
 import { compare } from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
-
 const schema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
@@ -25,12 +24,13 @@ export default async (req, res) => {
         const isMatch = await compare(password, checkUser.password);
         if (!isMatch) return res.status(401).json({ success: false, message: "Incorrect Password" });
 
-        const token = jwt.sign({ id: checkUser._id, email: checkUser.email }, process.env.JWT_SECREAT, { expiresIn: '1d' });
-        const finalData = { token, user: checkUser }
-        return res.status(200).json({ success: true, message: "Login Successfull", finalData })
+        // Using direct values for JWT_SECRET and other constants
+        const token = jwt.sign({ id: checkUser._id, email: checkUser.email }, '123456789vaibhav', { expiresIn: '1d' });
+        const finalData = { token, user: checkUser };
+        return res.status(200).json({ success: true, message: "Login Successful", finalData });
 
     } catch (error) {
-        console.log('Error in register (server) => ', error);
-        return res.status(500).json({ success: false, message: "Something Went Wrong Please Retry Later !" })
+        console.error('Error in login (server) => ', error);
+        return res.status(500).json({ success: false, message: "Something Went Wrong Please Retry Later!" });
     }
 }
